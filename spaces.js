ee28772923e7d49a1420f5f71dc16a7f3283ec79
Christian Bak <cbk@saxobank.com>
2014-08-19 04:14:58 +0200

    Bug fixes

--- spaces.js
+++ spaces.js
@@ -27,22 +27,24 @@ define(['jquery', 'less!style/spaces-component'], function($) {
         return targetEl;
     }
 
-    function toggleDisplay() {
+    function toggleDisplay(ev) {
+        if (!$(ev.target).hasClass('spaces-component')) { return; } //Only accept direct clicks on the togller
+
         var el = $(this);
 
         if (el.hasClass('collapsed')) {
-            $('#search-spaces').focus();
             el.removeClass('collapsed');
-            $('.container').removeClass('collapsed');
+            $('#search-spaces').focus();
         } else {
             el.addClass('collapsed');
-            $('.container').addClass('collapsed');
         }
     }
 
     function search(ev) {
         var searchTerm = this.value,
-            searchExp = new RegExp('(' + searchTerm + ')', 'gi');
+            searchExp = new RegExp('(' + searchTerm + ')', 'gi'),
+            firstResult;
+        
         $(this).closest('.container').find('div.org a').each(function() {
             var item = $(this);
 
@@ -55,19 +57,35 @@ define(['jquery', 'less!style/spaces-component'], function($) {
                 item.addClass('hidden');
             }
         });
+
+        firstResult = $(this).closest('.container').find('a:not(.hidden) em').first().parent();
+        $(this).closest('.container').find('a').removeClass('active');
+        firstResult.addClass('active');
+
+        if (ev.keyCode === 13 && firstResult.length) { //If enter is pressed, then go straight to the first search result
+            document.location.href = firstResult.attr('href');
+            console.log("13!");
+        }
     }
 
     function moveSelection(ev) {
         if (ev.keyCode === 8 && ev.target.nodeName !== "INPUT") { //Prevent backspace from navigating back
             ev.preventDefault();
         }
+
+        var container = $(this),
+            currentFocus,
+            allItems,
+            focusedIndex;
         
         if (ev.keyCode === 40 || ev.keyCode === 38) {
             ev.preventDefault();
-            var container = $(this),
-                currentFocus = container.find('a:focus'),
-                allItems = container.find('a:not(.hidden)'),
-                focusedIndex = allItems.index(currentFocus);
+            container = $(this),
+            currentFocus = container.find('a:focus'),
+            allItems = container.find('a:not(.hidden)'),
+            focusedIndex = allItems.index(currentFocus);
+
+            $(this).closest('.container').find('a').removeClass('active');
 
             if (ev.keyCode === 40) { //Down
                 $(allItems.get(focusedIndex + 1)).focus();
@@ -80,39 +98,43 @@ define(['jquery', 'less!style/spaces-component'], function($) {
                     container.find('input').select();
                 }
             }
+        } else { //If any other key is pressed, continue typing in the search field
+            container.find('input').focus();
         }
     }
 
     return $.fn.spaces = function(data) {
         return this.each(function() {
+
             var el = $(this),
-                container = $('<div class="container collapsed"/>')
-                    .appendTo($('body')),
-                results = $('<div class="results"/>');
+                container = $('<div class="container collapsed"/>'),
+                searcher = $('<div class="search-wrapper"><input type="text" name="search" id="search-spaces"></div>'),
+                results = $('<div class="results"/>'),
+                resultsFractionSize = 0.8;
             
-            results.css('max-height', window.outerHeight * 0.4 + 'px');
-            $(window).on('resize', function() {
-                results.css('max-height', window.outerHeight * 0.4 + 'px');
-            });
-
-            container.css('left', el.offset().left + 'px');
-            container.css('top', (el.offset().top + el.outerHeight()) + 'px');
-
             el.addClass('spaces-component');
             el.addClass('collapsed');
 
-            container.append('<div class="search-wrapper"><input type="text" name="search" id="search-spaces"></div>')
-                .find('input').on('keyup', search);
+            container.append(searcher);
+            searcher.find('input').on('keyup', search);
 
             container.on('keydown', moveSelection);
 
             el.click(toggleDisplay);
 
-            console.log(data);
             container.append(results);
             $.each(data, function(i, organization) {
                 renderOrg(organization).appendTo(results);
             });
+
+            el.append(container);
+
+            //////Hack to ensure that the result size is always at most 80% of the window size
+            results.css('max-height', ((window.outerHeight - searcher.outerHeight()) * resultsFractionSize - 100) + 'px');
+            $(window).on('resize', function() {
+                results.css('max-height', ((window.outerHeight - searcher.outerHeight()) * resultsFractionSize - 100) + 'px');
+            });
+            //////////////////
 
         });
     };
